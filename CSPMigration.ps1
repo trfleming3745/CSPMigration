33f67fd6b0f8dcd2e3fa52498f4b458bd30f1393
trfleming3745 <trfleming3745@gmail.com>
2017-09-02 16:35:45 -0700

    Initial CSPMigration Script

---
+++ CSPMigration.ps1
@@ -0,0 +1,28 @@
+﻿$creds = Get-Credential
+#Add-AzureAccount -Credential $creds
+
+Login-AzureRmAccount -Credential $creds
+$subs = Get-AzureRmSubscription | Select Name,SubscriptionID
+
+#Grabbing Resource Group Names from Non-CSP Subscription#
+Select-AzureRmSubscription -SubscriptionName $subs[0].Name 
+    $oldRSG = Get-AzureRmResourceGroup
+
+#Azure Inventory Assessment of Current Subscription to CSV#
+    Select-AzureRmSubscription -SubscriptionName $subs[0].Name
+    $Assessment =  @()
+    $Path = 'C:\Temp'
+    ForEach($RG in $oldRSG){
+        $Inventory = Get-AzureRmResource | where {$_.resourcegroupname -like $RG.resourcegroupname}
+        $Assessment += $Inventory
+    }
+    $Assessment | Export-Csv -Path "$($Path)\AzureInventory.csv" -NoTypeInformation 
+
+#Creating Identical Resource Groups in CSP Subscription#
+Select-AzureRmSubscription -SubscriptionName $subs[1].Name
+    foreach($RG in $oldRSG){
+        $RGCheck = Get-AzureRmResourceGroup -Name $RG.ResourceGroupName -ErrorAction SilentlyContinue
+        [boolean] $RGCheck
+        if($RGCheck -eq $false)
+            {Add-AzureRmResourceGroup -Name $RG.ResourceGroupName -Location $RG.Location}   
+    }
